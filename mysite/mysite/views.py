1d881ee877861dd24eeeac536c3de1efa678610c
ciya <ciya>
2017-09-29 08:52:03 +0200

    Created hours_ahead view.

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse #We import the class HttpResponse, which lives in the django.http module
+from django.http import HttpResponse, Http404 #We import the class HttpResponse and Http404, which live in the django.http module
 import datetime
 
 def hello(request): #We define a function called hello – the view function
@@ -8,3 +8,12 @@ def current_datetime(request): #The new current_datetime function calculates the
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now #This constructs an HTML response using Python’s “format-string” capability.        
     return HttpResponse(html) #Finally, the view returns an HttpResponse object that contains the generated response.
+
+def hours_ahead(request, offset): #offset is the string captured by the parentheses in the URLpattern.
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset) #you can perform date/time arithmetic by creating a datetime.timedelta object and adding to a datetime.datetime object
+    html = "<html><body>In %s hour(s), it will be  %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)

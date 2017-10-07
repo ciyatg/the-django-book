5d8927b1084cfb2f2a8e45c71d583af6ed95bc66
ciya <ciya>
2017-10-07 15:44:42 +0200

    created master template

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse, Http404 #We import the class HttpResponse and Http404, which live in the django.http module
 import datetime
+from django.template.loader import get_template
 
 def hello(request): #We define a function called hello – the view function
     return HttpResponse("Hello world") #Each view function takes at least one parameter, called request by convention.
 
 def current_datetime(request): #The new current_datetime function calculates the current date and time, as a datetime.datetime object, and stores that as the local variable now.
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now #This constructs an HTML response using Python’s “format-string” capability.        
+    t = get_template('current_datetime.html') #The get_template() function takes a template name as its argument, figures out where the template lives on the filesystem, opens that file, and returns a compiled Template object.
+    html = t.render({'current_date': now})        
     return HttpResponse(html) #Finally, the view returns an HttpResponse object that contains the generated response.
 
 def hours_ahead(request, offset): #offset is the string captured by the parentheses in the URLpattern.
